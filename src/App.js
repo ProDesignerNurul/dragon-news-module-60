0cfc7128eacc737d018d59d31de28c1a15e8a815
ProDesignerNurul <maxnewsbd@gmail.com>
2022-10-18 02:14:04 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './route/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      <h2>News Portal</h2>
+      <RouterProvider route={routes}></RouterProvider>
     </div>
   );
 }
